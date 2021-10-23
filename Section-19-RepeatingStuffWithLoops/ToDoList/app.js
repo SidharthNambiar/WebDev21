f23c48dae55ce9713d74d445b38c84923d8c069d
Sidharth Nambiar <snambiar01@gmail.com>
2021-10-23 12:16:46 -0400

    fixed bugs in todo list

--- Section-19-RepeatingStuffWithLoops/ToDoList/app.js
+++ Section-19-RepeatingStuffWithLoops/ToDoList/app.js
@@ -2,28 +2,29 @@ let todoList = [];
 const commandList = ["new", "list", "delete", "quit"];
 let response = prompt(
   "Welcome! This is your Todo List!\nCommands: new, list, delete, quit"
-).toLowerCase();
+).toLowerCase().trim();
 
 // get a valid entry
 while (!commandList.includes(response)) {
   response = prompt(
     "Invalid entry.\nPlease enter one the following commands: new, list, delete, quit"
-  ).toLowerCase();
+  ).toLowerCase().trim();
 }
 
 // run the program as long as user doesn't enter : quit
 
 while (response !== "quit") {
   if (response === "new") {
-    let itemToAdd = prompt("Enter item to add to todo list.");
+    let itemToAdd = prompt("Enter item to add to todo list.").trim();
     if (itemToAdd === "") {
-        response = prompt("Invalid Entry. Try another command: new, list, delete, quit")
+      response = prompt(
+        "Invalid Entry. Try another command: new, list, delete, quit"
+      ).trim();
     } else {
       todoList.push(itemToAdd);
       console.log(`${itemToAdd} added to the list`);
-      response = prompt("Try another command: new, list, delete, quit");
+      response = prompt("Try another command: new, list, delete, quit").trim();
     }
-
   } else if (response === "list") {
     if (todoList.length === 0) {
       console.log("Your todo list is empty!");
@@ -34,7 +35,7 @@ while (response !== "quit") {
       }
       console.log("******************************");
     }
-    response = prompt("Try another command: new, list, delete, quit");
+    response = prompt("Try another command: new, list, delete, quit").trim();
   } else if (response === "delete") {
     if (todoList.length === 0) {
       console.log("Your todo list is empty. Nothing to delete.");
@@ -45,10 +46,11 @@ while (response !== "quit") {
         validIndexList.push(i);
       }
 
-      response = prompt("Please enter the valid index of the item to delete'");
+      response = parseInt(prompt("Please enter the valid index of the item to delete'"));
 
       //validate the index entry
       while (!validIndexList.includes(response)) {
+        console.log(validIndexList);
         response = parseInt(prompt("Ivalid index entry. Try again!"));
       }
 
@@ -57,14 +59,14 @@ while (response !== "quit") {
       console.log("Entry deleted.");
     }
 
-    response = prompt("Try another command: new, list, delete, quit");
+    response = prompt("Try another command: new, list, delete, quit").trim();
   }
 
   // get a valid entry
   while (!commandList.includes(response)) {
     response = prompt(
       "Invalid entry.\nPlease enter one the following commands: new, list, delete, quit"
-    ).toLowerCase();
+    ).toLowerCase().trim();
   }
 }
 
